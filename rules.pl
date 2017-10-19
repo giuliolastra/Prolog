cc4807310df5f39ebe6d4eed993c963a5f987617
GIulio Lastra <giuliolastra@users.noreply.github.com>
2017-10-19 12:07:00 +0200

    Rules

--- rules.pl
+++ rules.pl
@@ -1,1 +1,13 @@
-rules.pl
+# Rules are the engine of prolog. They are composed of head and body.
+
+# Can be sintetized as: if the body is true the head is true too
+
+hungry(john).
+
+eat(john) :- hungry(jhon).
+
+# Query:
+
+#	eat(john).			yes.
+
+# 
